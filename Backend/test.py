84a9c098ae0be2b77244cee5c95f8612abbe703b
ahmedshifathedataengineer <hurricaneshadow726@gmail.com>
2024-11-02 12:41:22 -0400

    Changes to Backend, idea for  change to App.jsx

--- Backend/test.py
+++ Backend/test.py
@@ -1,1 +1,15 @@
 ###Test
+from flask import Flask, request, jsonify
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+def create_app():
+    #create an instance of application inside of the method
+
+    app = Flask(__name__)
+    
+    #return application
+    return app
+
+if __name__ == '__main__':
+    app.run(debug=True)
